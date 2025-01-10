7f233e89a5a3c2fc7c5ce3e14f6b62d09c8f8d75
Dark-luaa <darkkupal820@gmail.com>
2025-01-10 16:13:59 +0800

    Update deltav2.lua

--- deltav2.lua
+++ deltav2.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Dark-luaa/Hubv3/refs/heads/main/Dark.lua", true))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/D3athFeather/Script/main/Protected_023d4267eff0a5fb8082b0f7.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D3athFeather/Script/main/Protected_377eccb43a6f9e4dc31ea95d.lua", true))()
